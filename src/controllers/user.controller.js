c5b89a0240536cec76b693785e640cb8cb554a5f
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2022-05-29 21:10:35 +0700

    merge package from rifqi

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,46 +1,125 @@
-const register = (req, res) => {
-  return res.status(200).json({
-    status: true,
-    message: 'this is register endpoint',
-    results: req.body,
-  })
-}
+const datastore = require('../database/datastore');
+const nanoid = require('../config/nanoid');
+const jwt = require('jsonwebtoken');
 
-const login = (req, res) => {
-  return res.status(200).json({
-    status: true,
-    message: 'this is login endpoint',
-    results: req.body,
-  })
-}
+const register = async (req, res) => {
+  const { email } = req.body;
+  const key = datastore.key({
+    namespace: "Dev",
+    path: ["user"],
+  });
 
-const profile = (_, res) => {
-  return res.status(200).json({
-    results: {
-      id: 'eJXCDdzd',
-      username: 'testing',
-      email: 'testing@gmail.com',
-      picture: 'http://cloud-storage/profile/eJXCDdzd.png',
+  const entity = {
+    key,
+    data: {
+      ...req.body,
+      id: nanoid(),
+      createdAt: new Date().toJSON(),
+      updatedAt: new Date().toJSON(),
+      picture: "",
       origin: {
-        lat: -9.424724,
-        long: 105.2290617 
+        lat: 0,
+        lng: 0,
       },
-      shipping: [
-        {
-          id: 'gJDNQCOC',
-          cost: 10000,
-        },
-        {
-          id: '24SUZiRT',
-          cost: 15000,
-        },
-      ],
-      bookmarks: [
-        "pQLi6pat",
-        "8cLv0cul",
-      ]
+      passwordChangedAt: new Date().toJSON(),
+      bookmark: [],
+      shipping: [],
+      role: "user",
+    },
+  }
+
+  const query = datastore
+    .createQuery("Dev", "user")
+    .filter("email", "=", email);
+
+  const result = await datastore.runQuery(query);
+
+  if (result[0].length > 0) {
+    return res.status(409).json({
+      status: false,
+      message: 'Email has already registered',
+    })
+  }
+  
+  try {
+    const results = await datastore.save(entity);
+    return res.status(200).json({
+      status: true,
+      message: 'user successfully created',
+      results,
+    })
+  } catch (err) {
+    return res.status(400).json({
+      status: false,
+      message: '400 Bad Request',
+    })
+  }
+}
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const query = datastore
+    .createQuery("Dev", "user")
+    .filter("email", "=", email)
+    .filter("password", "=", password)
+    .limit(1);
+
+  try {
+    const result = await datastore.runQuery(query);
+
+    if (result[0].length === 0) {
+      return res.status(401).json({
+        status: false,
+        message: 'wrong username / password',
+      })
     }
-  })
+
+    const payload = {
+      id: result[0][0].id,
+      role: result[0][0].role,
+    }
+    const token = jwt.sign(payload, process.env.JWT_TOKEN);
+    return res.status(200).json({
+      status: true,
+      message: 'user successfully login',
+      token,
+    })
+  } catch (error) {
+    return res.status(400).json({
+      status: false,
+      message: '400 Bad Request',
+    })
+  }
+}
+
+const profile = async (req, res) => {
+  const query = datastore
+    .createQuery("Dev", "user")
+    .filter("id", "=", req.user.id)
+    .limit(1);
+
+  try {
+    const result = await datastore.runQuery(query);
+
+    if (result[0].length === 0) {
+      return res.status(404).json({
+        status: false,
+        message: '404 Not Found',
+      })
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: 'success',
+      results: result[0][0],
+    })
+  } catch (err) {
+    return res.status(400).json({
+      status: false,
+      message: '400 Bad Request',
+    })
+  }
 }
 
 const update = (req, res) => {
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const user = require('../controllers/user.controller');
+const jwtMiddleware = require('../middlewares/jwtAuth');
 
 router.post('/register', user.register);
 
 router.post('/login', user.login);
 
-router.get('/profile', user.profile);
+router.get('/profile',jwtMiddleware, user.profile);
 
 router.put('/update', user.update);
 
